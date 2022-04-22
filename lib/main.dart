9cf2896778600cd7a8d6c3136d684b415a9b7a14
mrbrownfr <elouan.marron@gmail.com>
2022-04-22 11:42:51 +0200

    Organisation du code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Pages/home.dart';
+import 'Pages/settings.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,33 +17,4 @@ void main() {
     darkTheme: ThemeData.dark(),
     themeMode: ThemeMode.light,
   ));
-}
-
-class AppHome extends StatelessWidget {
-  const AppHome({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Accueil',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add),
-          label: 'Ajouter',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Rechercher',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Paramètres',
-        ),
-      ],
-    );
-  }
 }
