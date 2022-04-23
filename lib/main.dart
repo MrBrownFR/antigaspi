7526b81f91a5ffca8ba469f93d3a2a838fb0f396
mrbrownfr <elouan.marron@gmail.com>
2022-04-23 11:25:47 +0200

    Ajout de Firebase (config de base)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:antigaspi/Pages/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     title: 'AntiGaspi LDV',
     home: const HomePage(),
